75c3756218bc22fc2d33d473ae6ca78fa9fbda26
Dayane Chiozzi <daychiozzi@gmail.com>
2022-10-24 19:13:20 -0300

    :recycle:refactor: right function , deleted if

--- app/backend/src/JWT/token.ts
+++ app/backend/src/JWT/token.ts
@@ -1,13 +1,10 @@
 import 'dotenv/config';
 import { sign } from 'jsonwebtoken';
 
-const { JWT_SECRET } = process.env;
+const secret = process.env.JWT_SECRET as string;
 
 const createToken = (email:string) => {
-  if (!JWT_SECRET) {
-    throw new Error('Secret not Found');
-  }
-  const token = sign({ email }, JWT_SECRET, { algorithm: 'HS256', expiresIn: '10d' });
+  const token = sign({ email }, secret, { algorithm: 'HS256', expiresIn: '10d' });
   return token;
 };
 
