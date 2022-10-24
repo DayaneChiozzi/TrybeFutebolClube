9d39aba6481d9f31d83debcfb900c5a153d755ad
Dayane Chiozzi <daychiozzi@gmail.com>
2022-10-24 20:49:00 -0300

    :recycle:refactor: includ if password, status 404

--- app/backend/src/middleware/ValidateLogin.ts
+++ app/backend/src/middleware/ValidateLogin.ts
@@ -5,12 +5,15 @@ class ValidateLogin {
   loginService = new LoginService();
 
   public loginVerify = async (req: Request, res: Response, next: NextFunction) => {
-    const { email } = req.body;
+    const { email, password } = req.body;
     const result = await this.loginService.login(email);
-    if (!result?.email || !result.password) {
+
+    if (!email || !password) {
       return res.status(400).json({ message: 'All fields must be filled' });
     }
-    // console.log('validateLogin', result);
+    if (!result) {
+      return res.status(404).json({ message: 'email or password not found!' });
+    }
     next();
   };
 }
