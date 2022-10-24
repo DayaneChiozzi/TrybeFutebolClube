d9b677eef409c7dad38d95591d75ad891e6d733b
Dayane Chiozzi <daychiozzi@gmail.com>
2022-10-24 19:10:26 -0300

    :recycle:refactor: deleted const generateToken

--- app/backend/src/service/LoginService.ts
+++ app/backend/src/service/LoginService.ts
@@ -1,20 +1,17 @@
 import UserModel from '../database/models/UserModel';
-import IToken from '../interface/IToken';
 import IUser from '../interface/IUser';
-import Token from '../JWT/token';
 
 class LoginService {
   public model = UserModel;
 
-  public login = async (email: string, password: string):Promise<IToken | string | undefined> => {
+  public login = async (email: string):Promise<IUser | undefined> => {
     try {
-      const resultUser = await this.model.findOne({ where: { email, password } }) as IUser;
-      console.log('SERVICE', resultUser);
+      const resultUser = await this.model.findOne({
+        where: { email }, raw: true,
+      }) as IUser;
       if (!resultUser) {
         throw new Error('email not found');
-      }
-      const generatedToken = Token.createToken(email);
-      return generatedToken;
+      } return resultUser;
     } catch (error) {
       console.error(error);
     }
