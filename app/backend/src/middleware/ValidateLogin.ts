4cb940b9e80805e55c4528091805a69fa872a15e
Dayane Chiozzi <daychiozzi@gmail.com>
2022-10-26 00:24:02 -0300

    :recycle:refactor: change for status and message

--- app/backend/src/middleware/ValidateLogin.ts
+++ app/backend/src/middleware/ValidateLogin.ts
@@ -6,13 +6,13 @@ class ValidateLogin {
 
   public loginVerify = async (req: Request, res: Response, next: NextFunction) => {
     const { email, password } = req.body;
-    const result = await this.loginService.login(email);
 
     if (!email || !password) {
       return res.status(400).json({ message: 'All fields must be filled' });
     }
+    const result = await this.loginService.login(email, password);
     if (!result) {
-      return res.status(404).json({ message: 'email or password not found!' });
+      return res.status(401).json({ message: 'Incorrect email or password' });
     }
     next();
   };
