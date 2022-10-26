f57bbaae04a9293b1c298d0b2454f843b810edc9
Dayane Chiozzi <daychiozzi@gmail.com>
2022-10-26 00:25:23 -0300

    :recycle:refactor:add CustomError, parametro password, bcrypt

--- app/backend/src/service/LoginService.ts
+++ app/backend/src/service/LoginService.ts
@@ -1,19 +1,34 @@
+import * as bcrypt from 'bcryptjs';
+import CustomError from '../error/CustomError';
 import UserModel from '../database/models/UserModel';
 import IUser from '../interface/IUser';
 
 class LoginService {
   public model = UserModel;
 
-  public login = async (email: string):Promise<IUser | undefined> => {
+  public login = async (email: string, password: string):Promise<IUser | undefined> => {
     try {
       const resultUser = await this.model.findOne({
         where: { email }, raw: true,
       }) as IUser;
-      if (!resultUser) {
-        throw new Error('email not found');
-      } return resultUser;
+      const passBcrypt = await bcrypt.compare(password, resultUser.password);
+
+      if (!passBcrypt) {
+        throw new CustomError(401, 'Incorrect email or password');
+      }
+      return resultUser;
     } catch (error) {
       console.error(error);
+    }
+  };
+
+  public getLoginValidate = async (userEmail: string) => {
+    try {
+      const role = await this
+        .model.findOne({ where: { email: userEmail }, raw: true, attributes: ['role'] });
+      return role;
+    } catch (error) {
+      console.log(error);
     }
   };
 }
