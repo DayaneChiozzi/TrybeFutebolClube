a865787f49f3999af3e28c4e2b7fa06a77e9eead
Dayane Chiozzi <daychiozzi@gmail.com>
2022-10-27 10:35:09 -0300

    :sparles:feat: model Team

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,28 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  public id!: number;
+  public teamName!: string;
+}
+
+Team.init({
+  id: {
+    allowNull: false,
+    primaryKey: true,
+    type: INTEGER,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  tableName: 'teams',
+  timestamps: false,
+});
+
+export default Team;
