db34c5c55d327f0e9c78613c5fa8a63cf01629ee
Dayane Chiozzi <daychiozzi@gmail.com>
2022-10-27 20:00:55 -0300

    :recycle:refactor: matches controller add function create

--- app/backend/src/controller/matchesController.ts
+++ app/backend/src/controller/matchesController.ts
@@ -10,6 +10,11 @@ class MatchesController {
     const matchesService = await this.matchesService.getAll(inProgress);
     return res.status(200).json(matchesService);
   };
+
+  public create:RequestHandler = async (req, res) => {
+    const salveMatch = await this.matchesService.create(req.body);
+    return res.status(201).json(salveMatch);
+  };
 }
 
 export default MatchesController;
