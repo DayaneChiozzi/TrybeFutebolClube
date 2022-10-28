00b734e34c4161d7116a58f1c23e616891c87429
Dayane Chiozzi <daychiozzi@gmail.com>
2022-10-28 12:36:32 -0300

    :recycle:refactor: id armazenado em uma const

--- app/backend/src/middleware/validateMatches.ts
+++ app/backend/src/middleware/validateMatches.ts
@@ -5,11 +5,14 @@ const { getByPk } = new TeamsService();
 
 const validateMatches:RequestHandler = async (req, res, next) => {
   const { homeTeam, awayTeam } = req.body;
+  const teamHome = await getByPk(homeTeam);
+  const teamAway = await getByPk(awayTeam);
+
   if (homeTeam === awayTeam) {
     return res.status(422)
       .json({ message: 'It is not possible to create a match with two equal teams' });
   }
-  if (!await getByPk(homeTeam) || !await getByPk(awayTeam)) {
+  if (!teamHome || !teamAway) {
     return res.status(404).json({ message: 'There is no team with such id!' });
   }
   next();
